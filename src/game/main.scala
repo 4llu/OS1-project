427c267fd1105e886338deddc740df7bbb3f6ed0
MusserO <iiro.kumpulainen@netikka.fi>
2016-11-16 14:50:01 +0200

    Added window

--- src/game/main.scala
+++ src/game/main.scala
@@ -6,5 +6,39 @@ import scala.swing._
   * Created by Allu on 04/11/2016.
   */
 object main extends SimpleSwingApplication {
+  
+  def top = new MainFrame {
+    this.peer.addKeyListener(new KeyListener() {
+        def keyPressed(e:KeyEvent) {
+          println("key pressed: "+e.getKeyChar)
+//          if (e.getKeyChar.toLower.equals('w')) {
+//            cameraY -= 50
+//          } else if (e.getKeyChar.toLower.equals('a')) {
+//            cameraX -= 50
+//          } else if (e.getKeyChar.toLower.equals('s')) {
+//            cameraY += 50
+//          } else if (e.getKeyChar.toLower.equals('d')) {
+//            cameraX += 50
+//          }
+        }
+    
+        def keyReleased(e:KeyEvent) {
+          println("key released: "+e.getKeyChar)
+        }
+    
+        def keyTyped(e:KeyEvent) {
+          println("key typed: "+e.getKeyChar)
+        }
+     })
+    contents = new BoxPanel(Orientation.NoOrientation) {
+      contents += panel
+    }
+  }
     game.start(1, Hard)
+}
+
+class DataPanel extends Panel {
+  override def paintComponent(g: Graphics2D) {
+    g.clearRect(0, 0, size.width, size.height)
+  }
 }
