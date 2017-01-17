a53d7cf8afc1396beaaf148b1de971013d6fe1ae
Aleksanteri Hämäläinen <aleksanterih@gmail.com>
2017-01-18 01:16:44 +0200

    Made changes to fireballs stats

--- src/game/Fireball.scala
+++ src/game/Fireball.scala
@@ -12,7 +12,7 @@ import javax.imageio.ImageIO
 // FIXME Should direction be Projectiles parameter?
 class Fireball(x:Int, y:Int, world:World, var direction: Direction) extends Projectile(x, y, world) {
   var sprite: BufferedImage = ImageIO.read(new File("media/fireball.png"))
-  var speed = 2.0f
+  var speed = 1.0f
   var location = new Location(x, y, sprite.getWidth, sprite.getHeight, world)
 
 }
