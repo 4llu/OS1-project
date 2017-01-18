70b71b0276c9f0955d7885ddc27e3736e434e32a
Aleksanteri Hämäläinen <aleksanterih@gmail.com>
2017-01-18 17:55:00 +0200

    Fireballs now die when they are blocked

--- src/game/Fireball.scala
+++ src/game/Fireball.scala
@@ -8,11 +8,15 @@ import javax.imageio.ImageIO
   * Created by Allu on 16/01/2017.
   */
 
-// FIXME location should be defined in Projectile, but that throws a "Reassignment to val" -error
-// FIXME Should direction be Projectiles parameter?
 class Fireball(x:Int, y:Int, world:World, var direction: Direction) extends Projectile(x, y, world) {
   var sprite: BufferedImage = ImageIO.read(new File("media/fireball.png"))
   var speed = 1.0f
   var location = new Location(x, y, sprite.getWidth, sprite.getHeight, world)
+
+  override def update(timeElapsed: Long): Unit = {
+    super.update(timeElapsed)
+
+    if (this.blocked) this.remove = true
+  }
 
 }
