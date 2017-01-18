612ac6def2eddf7d7e762bc39db4946f5080fdfc
Aleksanteri Hämäläinen <aleksanterih@gmail.com>
2017-01-18 22:11:11 +0200

    Grass maps

--- src/game/World.scala
+++ src/game/World.scala
@@ -15,18 +15,28 @@ class World(worldNum: Int) {
   private val flowersImage = Canvas.combineImages(grassImage, flowers, true)
   private val bush = objectTileSet.getSubimage(160, 64, 64, 64)
   private val bushImage = Canvas.combineImages(grassImage, bush, true)
+  // Cliffs sides
   private val cliffDown = cliffTileSet.getSubimage(96, 128, 64, 96)
   private val cliffDownImage = Canvas.combineImages(grassImage, cliffDown, false)
   private val cliffUp = cliffTileSet.getSubimage(96, 0, 64, 64)
   private val cliffUpImage = Canvas.combineImages(grassImage, cliffUp, true)
   private val cliffLeft = cliffTileSet.getSubimage(32, 64, 64, 64)
   private val cliffLeftImage = Canvas.combineImages(grassImage, cliffLeft, true)
-  private val cliffRight = cliffTileSet.getSubimage(160, 64, 64, 64)
+  private val cliffRight = cliffTileSet.getSubimage(160, 80, 64, 64)
   private val cliffRightImage = Canvas.combineImages(grassImage, cliffRight, true)
-  
+  // Cliff corners
+  private val cliffUpLeft = cliffTileSet.getSubimage(32, 0, 64, 96)
+  private val cliffUpLeftImage = Canvas.combineImages(grassImage, Canvas.combineImages(grassImage, cliffUpLeft, true), false)
+  private val cliffUpRight = cliffTileSet.getSubimage(160, 0, 64, 96)
+  private val cliffUpRightImage = Canvas.combineImages(grassImage, cliffUpRight, false)
+  private val cliffDownRight = cliffTileSet.getSubimage(160, 128, 64, 96)
+  private val cliffDownRightImage = Canvas.combineImages(grassImage, Canvas.combineImages(grassImage, cliffDownRight, true), false)
+  private val cliffDownLeft = cliffTileSet.getSubimage(32, 128, 64, 96)
+  private val cliffDownLeftImage = Canvas.combineImages(grassImage, cliffDownLeft, false)
+
   val backgroundImage = grassImage
   
-  val map = ArrayBuffer(  
+  val map1 = ArrayBuffer(
     "U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+",  
     "++++++++++++++++++++++++++++++++++",  
     "B+,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.B+",  // B+
@@ -62,6 +72,107 @@ class World(worldNum: Int) {
     "D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+", 
     "++++++++++++++++++++++++++++++++++", 
     "++++++++++++++++++++++++++++++++++")
+
+  val map2 = ArrayBuffer(
+    "1+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+2+",
+    "++++++++++++++++++++++++++++++++++++++++++++++++++++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++................................................++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++................................................++",
+    "L+,.,.,.,.1+U+U+U+U+2+,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++........++........++............................++",
+    "L+,.,.,.,.L+,.,.,.,.R+,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++........++........++............................++",
+    "L+,.,.,.,.L+,.,.,.,.R+,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++........++........++............................++",
+    "L+,.,.,.,.L+,.,.,.,.R+,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++........++........++............................++",
+    "L+,.,.,.,.4+D+D+D+D+3+,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++........++++++++++++............................++",
+    "L+,.,.,.,.++++++++++++,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++................................................++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++................................................++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++................................................++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++................................................++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++................................................++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++................................................++",
+    "4+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+3+",
+    "++++++++++++++++++++++++++++++++++++++++++++++++++++",
+    "++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+  val map = ArrayBuffer(
+    "1+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+2+",
+    "++++++++++++++++++++++++++++++++++++++++++++++++++++",
+    "L+,f,.,.,f,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++......f......f......ff........ff.........f......++",
+    "L+,.,f,.,.,f,.,.,.,.,.,.,.,.,.,.,f,.,.,.,.,.,.,.,.R+",
+    "++................................................++",
+    "L+B+B+B+B+B+B+B+B+B+,.,.,f,.,.,.B+B+B+B+B+B+B+B+B+R+",
+    "++++++++++++++++++++............++++++++++++++++++++",
+    "L+,.,f,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++....f.......f...........f...............f.......++",
+    "L+,.,.,.,.,.,f,.,.,.,.,.,.,.,.,.,.,.,.,.,f,.,f,.,.R+",
+    "++...f.............f........ff.............f......++",
+    "L+,.,.,.,f,.,.,.,.,.,.,.,f,.,.,.,.,.,f,.,.,.,.,.,.R+",
+    "++...................f............................++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,f,.,.,.,.,.,.,.,.,f,.,.R+",
+    "++................f..............f................++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,f,.,.,.,.,.R+",
+    "++.........................f.........f............++",
+    "L+B+B+B+B+B+B+B+B+B+,.,.,.,f,.,.B+B+B+B+B+B+B+B+B+R+",
+    "++++++++++++++++++++.....f......++++++++++++++++++++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++.................f..............................++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,f,.,.,.,.,.,.,.,.,f,.,.,.R+",
+    "++......f....................f............f......++",
+    "L+,.,.,.,.,.,.,.,f,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++...f.....................f........f.............++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,f,.,.,.,.,.,.R+",
+    "++................................................++",
+    "4+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+3+",
+    "++++++++++++++++++++++++++++++++++++++++++++++++++++",
+    "++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+
+  // Blank grass map
+  val map3 = ArrayBuffer(
+    "1+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+U+2+",
+    "++++++++++++++++++++++++++++++++++++++++++++++++++++",
+    "L+,f,.,.,f,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++......f......f......ff........ff.........f......++",
+    "L+,.,f,.,.,f,.,.,.,.,.,.,.,.,.,.,f,.,.,.,.,.,.,.,.R+",
+    "++................................................++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,f,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++................................................++",
+    "L+,.,f,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++....f.......f...........f...............f.......++",
+    "L+,.,.,.,.,.,f,.,.,.,.,.,.,.,.,.,.,.,.,.,f,.,f,.,.R+",
+    "++...f.............f........ff.............f......++",
+    "L+,.,.,.,f,.,.,.,.,.,.,.,f,.,.,.,.,.,f,.,.,.,.,.,.R+",
+    "++...................f............................++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,f,.,.,.,.,.,.,.,.,f,.,.R+",
+    "++................f..............f................++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,f,.,.,.,.,.R+",
+    "++.........................f.........f............++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,f,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++.......................f........................++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++.................f..............................++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,f,.,.,.,.,.,.,.,.,f,.,.,.R+",
+    "++......f....................f............f......++",
+    "L+,.,.,.,.,.,.,.,f,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.R+",
+    "++...f.....................f........f.............++",
+    "L+,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,f,.,.,.,.,.,.R+",
+    "++................................................++",
+    "4+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+D+3+",
+    "++++++++++++++++++++++++++++++++++++++++++++++++++++",
+    "++++++++++++++++++++++++++++++++++++++++++++++++++++")
     
   private val tileWidth = 32
   private val tileHeight = 32
@@ -100,8 +211,26 @@ class World(worldNum: Int) {
           tiles(y)(x) = new Tile(cliffDownImage, 
               new Location(x*tileWidth, y*tileHeight, tileWidth*2, tileHeight*2, this), false, "cliffdown")
         } else if (tile == 'U') {
-          tiles(y)(x) = new Tile(cliffUpImage, 
-              new Location(x*tileWidth, y*tileHeight, tileWidth*2, tileHeight*2, this), false, "cliffdown")
+          tiles(y)(x) = new Tile(cliffUpImage,
+              new Location(x*tileWidth, y*tileHeight, tileWidth*2, tileHeight*2, this), false, "cliffup")
+        } else if (tile == 'R') {
+          tiles(y)(x) = new Tile(cliffRightImage,
+            new Location(x*tileWidth, y*tileHeight, tileWidth*2, tileHeight*2, this), false, "cliffright")
+        } else if (tile == 'L') {
+          tiles(y)(x) = new Tile(cliffLeftImage,
+            new Location(x*tileWidth, y*tileHeight, tileWidth*2, tileHeight*2, this), false, "cliffleft")
+        } else if (tile == '1') {
+          tiles(y)(x) = new Tile(cliffUpLeftImage,
+            new Location(x*tileWidth, y*tileHeight, tileWidth*2, tileHeight*2, this), false, "cliffupleft")
+        } else if (tile == '2') {
+          tiles(y)(x) = new Tile(cliffUpRightImage,
+            new Location(x*tileWidth, y*tileHeight, tileWidth*2, tileHeight*2, this), false, "cliffupright")
+        } else if (tile == '3') {
+          tiles(y)(x) = new Tile(cliffDownRightImage,
+            new Location(x*tileWidth, y*tileHeight, tileWidth*2, tileHeight*2, this), false, "cliffdownright")
+        } else if (tile == '4') {
+          tiles(y)(x) = new Tile(cliffDownLeftImage,
+            new Location(x*tileWidth, y*tileHeight, tileWidth*2, tileHeight*2, this), false, "cliffdownleft")
         } else if (tile == '+') { // The other corners of the 64x64 tile
           tiles(y)(x) = new Tile(null, 
               new Location(x*tileWidth, y*tileHeight, tileWidth*2, tileHeight*2, this), false, "extension")
