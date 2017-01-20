3affb237023f5c9dbf2c91f3848a8f194703ad20
Aleksanteri Hämäläinen <aleksanterih@gmail.com>
2017-01-20 17:03:03 +0200

    Increased ice spell speed

--- src/game/IceShard.scala
+++ src/game/IceShard.scala
@@ -11,7 +11,7 @@ import scala.collection.mutable.ArrayBuffer
   */
 class IceShard(x:Int, y:Int, world:World, var direction: Direction, hitsMonsters: Boolean, hitsPlayer: Boolean) 
   extends Projectile(x, y, world, hitsMonsters, hitsPlayer) {
-  var speed = 1.0f
+  var speed = 1.2f
   val damage = 25
   var sprite: BufferedImage = ImageIO.read(new File("media/freezeball.png"))
   var location = new Location(x, y, sprite.getWidth, sprite.getHeight, world)
